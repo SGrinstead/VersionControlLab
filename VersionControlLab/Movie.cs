009570bb0ed7de7cb607059bea603670eda5c688
SGrinstead <seth.grinstead3299@gmail.com>
2023-05-08 13:37:59 -0700

    Movie properties and Constructor

---
+++ VersionControlLab/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionControlLab
+{
+    public class Movie
+    {
+        public string Title;
+        public string Director;
+        public int Year;
+
+        public Movie(string title, string director, int year)
+        {
+            Title = title;
+            Director = director;
+            Year = year;
+        }
+    }
+}
