8f65d020e367fcda399516b84119d48a752cd64f
SGrinstead <seth.grinstead3299@gmail.com>
2023-05-08 13:43:21 -0700

    Movie Test

---
+++ VersionControlLab_UnitTests/MovieTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionControlLab
+{
+    public class MovieTests
+    {
+        [Fact]
+        public void Movie_Constructor_InitializesTitleDirectorAndYear()
+        {
+            Movie fuzz = new Movie("Hot Fuzz", "Edgar Wright", 2007);
+
+            Assert.Equal("Hot Fuzz", fuzz.Title);
+            Assert.Equal("Edgar Wright", fuzz.Director);
+            Assert.Equal(2007, fuzz.Year);
+        }
+    }
+}
