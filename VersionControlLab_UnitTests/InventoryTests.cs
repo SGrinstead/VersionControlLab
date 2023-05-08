48aa22ab37a1ec67bd679291fc9e8da4050fbc0b
SGrinstead <seth.grinstead3299@gmail.com>
2023-05-08 14:08:03 -0700

    Inventory tests

--- VersionControlLab_UnitTests/InventoryTests.cs
+++ VersionControlLab_UnitTests/InventoryTests.cs
@@ -12,10 +12,49 @@ namespace VersionControlLab
         }
 
         [Fact]
+        public void Inventory_AddBook_AddsBookToBooks()
+        {
+            Inventory inventory = new Inventory();
+            Book lotr = new Book("Lord of the Rings", "JRR Tolkien");
+            
+            inventory.AddBook(lotr);
+
+            Assert.Equal(lotr, inventory.Books[0]);
+        }
+
+        [Fact]
+        public void Inventory_AddMovie_AddsMovieToMovies()
+        {
+            Inventory inventory = new Inventory();
+            Movie fuzz = new Movie("Hot Fuzz", "Edgar Wright", 2007);
+
+            inventory.AddMovie(fuzz);
+
+            Assert.Equal(fuzz, inventory.Movies[0]);
+        }
+
+        [Fact]
         public void Inventory_StealBook_RemovesBookFromBooks()
         {
             Book lotr = new Book("Lord of the Rings", "JRR Tolkien");
             Inventory inventory = new Inventory();
+
+            inventory.AddBook(lotr);
+            inventory.StealBook(lotr);
+
+            Assert.Empty(inventory.Books);
+        }
+
+        [Fact]
+        public void Inventory_StealMovie_RemovesMovieFromMovies()
+        {
+            Movie fuzz = new Movie("Hot Fuzz", "Edgar Wright", 2007);
+            Inventory inventory = new Inventory();
+
+            inventory.AddMovie(fuzz);
+            inventory.StealMovie(fuzz);
+
+            Assert.Empty(inventory.Movies);
         }
     }
 }
