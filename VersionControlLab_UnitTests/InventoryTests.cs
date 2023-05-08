b01da346501a5a198611b4dd7f97f8fb6cffce0c
SGrinstead <seth.grinstead3299@gmail.com>
2023-05-08 13:50:04 -0700

    Inventory constructor test

--- VersionControlLab_UnitTests/InventoryTests.cs
+++ VersionControlLab_UnitTests/InventoryTests.cs
@@ -3,9 +3,12 @@ namespace VersionControlLab
     public class InventoryTests
     {
         [Fact]
-        public void Test1()
+        public void Inventory_Constructor_InitializesBooksAndMovies()
         {
+            Inventory inventory = new Inventory();
 
+            Assert.Equal(new List<Book>(), inventory.Books);
+            Assert.Equal(new List<Movie>(), inventory.Movies);
         }
     }
 }
